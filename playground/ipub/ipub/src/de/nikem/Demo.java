fc371745435c56c9db1d6926dd2f26d8dd5d9348
andi <andreas.knees@nikem.de>
2014-03-04 05:17:54 +0100

    Demo

--- playground/ipub/ipub/src/de/nikem/Demo.java
+++ playground/ipub/ipub/src/de/nikem/Demo.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -19,38 +18,47 @@ import org.json.JSONWriter;
 import de.nikem.dataj.ListPage;
 import de.nikem.dataj.jq.JqPaginationQuery;
 
+
 public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 5056999204692421446L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
 		try {
-			DataSource dataSource = (DataSource) InitialContext.doLookup("java:comp/env/JDBC");
+			Object o = InitialContext.doLookup("");
+			System.out.println(o);
+			
+			DataSource dataSource = (DataSource) InitialContext.doLookup("java:/comp/env/jdbc/reanim8or");
+			resp.setCharacterEncoding("UTF-8");
+			resp.setHeader("Content-Type", "application/json;charset=utf-8");
 			final Writer writer = resp.getWriter();
 			final JSONWriter w = new JSONWriter(writer);
 			w.object();
 			
 			w.key("aaData");
-			ListPage<Object> page = new JqPaginationQuery<Object>(dataSource, "select * from employees", req.getParameterMap()) {
+			w.array();
+			ListPage<Object> page = new JqPaginationQuery<Object>(dataSource, "select autor_name AUTORNAME, autor_vorname AUTORVORNAME, jahr JAHR from ipub_publications", req.getParameterMap()) {
 				@Override
 				protected Object mapRow(ResultSet rs) throws SQLException {
-					w.key("FIRST_NAME").value(rs.getString("FIRST_NAME"));
-					w.key("LAST_NAME").value(rs.getString("LAST_NAME"));
-					w.key("BIRTHDAY").value(new SimpleDateFormat().format(rs.getDate("BIRTHDAY")));
+					w.object();
+					w.key("FIRST_NAME").value(rs.getString("AUTORNAME"));
+					w.key("LAST_NAME").value(rs.getString("AUTORVORNAME"));
+					w.key("JAHR").value(rs.getInt("JAHR"));
 					//... and so on
+					w.endObject();
 					return null; //return value not needed because content is directly put to JSON output
 				}
 			}.execute();
-
+			w.endArray();
 			w.key("iTotalRecords").value(page.getTotalRecords());
 			w.key("iTotalDisplayRecords").value(page.getTotalDisplayRecords());
 			w.key("sEcho").value(page.getEcho());
 			w.endObject();
 		} catch (SQLException ex) {
-			throw new ServletException("DB access failed");
+			throw new ServletException("DB access failed", ex);
 		} catch (NamingException e) {
-			throw new ServletException("Lookup failed");
+			throw new ServletException("Lookup failed", e);
 		}
 	}
 }
